022ed13a196a22701936b59ee313971e43266fa7
UIT-20521205 <dinhiendung10@gmail.com>
2023-04-01 21:23:34 +0700

    Complete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BMICalculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        float height;
+        float weight;
+
+        Console.Write("Please enter a height: ");
+        height = float.Parse(Console.ReadLine());
+        Console.Write("Please enter your weight: ");
+        weight = float.Parse(Console.ReadLine());
+        float bmi = weight / (float)Math.Pow(height, 2);
+        bmi = (float)Math.Round(bmi, 2);
+        Console.WriteLine($"{BMICalculator(bmi)}!");
+    }
+
+    public static string BMICalculator(float bmi)
+    {
+        if (bmi < 18.5) return "Underweight";
+        else if (bmi >= 18.5 && bmi <= 25) return "Normal";
+        else if (bmi >= 25 && bmi <= 30) return "Overweight";
+        else return "Obese";
+    }
+}
